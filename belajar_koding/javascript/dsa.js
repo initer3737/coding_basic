06310ffaebc3a849ca611278e0efdfb71ab80f72
initer3737 <puthutwahyuaji13@gmail.com>
2026-02-19 16:19:30 +0700

    update

--- belajar_koding/javascript/dsa.js
+++ belajar_koding/javascript/dsa.js
@@ -1,3 +1,43 @@
+//todo buat observer untuk mendeteksi perubahan data terkini dan reactive
+//buat tiruan dari react js usestate 
+const useState=(data)=>{
+ const reactive_data=[]
+  //ketika ada perubahan data maka 
+  //todo data diset misa jadi 2 maka otomatis data 
+  //akan diupdate jadi nilai yang baru
+ //reactive_data variable menampung parameter dan nanti akan diinvoke 
+  //ketika set data dipanggil dan nanti otomatis akan mengupdate data 
+  //secara reactive
+  //jika param data itu fungsi maka setdata(val=>val+1)
+  const watcher=val=>{
+      reactive_data.push(val);
+      
+  }
+  const setdata=(new_val)=>{
+       let data;
+      if(typeof new_val=== "function"){
+            //jika new_val adalah fungsi maka bisa diambil preval pakai callback
+             data_new=new_val(data)
+          return reactive_data[0]=data_new     
+      }
+
+    return reactive_data[0]=new_val  
+  }
+  //ambil data pertama saat data pertama kali diinisialisasi
+ let get_data=data
+  //ambil data terakhir di array untuk mendapatkan data reactive
+  const newest_data=()=>{
+      watcher(val=>{
+        return val 
+      })
+  }
+
+  return [ newest_data(),setdata]
+  
+}
+
+
+
 const useParams=()=>{
     const url="shoping.com/product/12"
     const route="shoping.com/product/:id"
@@ -7,7 +47,7 @@ const useParams=()=>{
             const matching=route.match(/:\w+/g)
                 if(!matching)return {}
             const result=matching.reduce((acc,cur,index)=>{
-                const key=cur.replace(/:/g,"")
+                const key=cur
                 const index_route=route.split("/").indexOf(key)
                 
                 acc[key]=url_split[index_route]
@@ -18,18 +58,54 @@ const useParams=()=>{
 
 }
 
+//todo i want to perform asynchronus request 
+//to json file so i will create that json file
+//making library to handle request 
+const axios={
+  get:async({url})=>{
+    
+  },
+  post:async({url})=>{
 
+  },
+
+}
+
+
+
+//todo saya ingin memmbuat library react sederhana cloning ajah 
+//jadi saya mengkonversi beberapa fungsi jadi custom element 
+//dan passing props seperti react 
+// contohnya fungsi Product maka akan jadi <Product>
 
 const shopping=()=>{
+
     
     const database=[
         {id:1,product:"adidas shoes"},
         {id:2,product:"pillow"},
     ]
+
 }
   
 //react look a like 
-//use param name fungsi akan dikonversi jadi custom element syarat yang huruf pertamanya adalah uppercase
+//use param name fungsi akan dikonversi jadi custom element 
+//syarat yang huruf pertamanya adalah uppercase
+//fungsi dibawah adalah mendaftarkan framework sebagai custom 
+//element dan menggabungkan props di agar bisa diakses oleh fungsi
 const register_framework_spa=()=>{
 
 }
+
+//todo 
+//
+//
+//
+//try use state 
+
+const [counter,setCounter]=useState(0)
+
+setCounter(val=>val+10)
+setCounter(90)
+console.log("counter is ",counter)
+
